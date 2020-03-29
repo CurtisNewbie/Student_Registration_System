64d506f48712b5cb7dae87878096c67b63b9aae2
CurtisNewbie <zhuangyongj@gmail.com>
2020-03-29 05:11:21 +0100

    Project init

---
+++ stu_reg_sys/src/main/java/com/curtisnewbie/main/App.java
@@ -0,0 +1,54 @@
+package com.curtisnewbie.main;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ * ------------------------------------
+ * 
+ * Author: Yongjie Zhuang
+ * 
+ * ------------------------------------
+ * <p>
+ * Javafx Application
+ * </p>
+ */
+public class App extends Application {
+
+    /** Primary Stage of Javafx App, may be used to create alerts. */
+    static Stage primaryStage;
+
+    /** Parent in FXML that will be loaded */
+    private Parent root;
+
+    private final String TITLE = "Student Registration System";
+
+    @Override
+    public void init() throws Exception {
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        App.primaryStage = primaryStage;
+
+        Scene s = new Scene(new Pane());
+        primaryStage.setScene(s);
+        primaryStage.setTitle(TITLE);
+        primaryStage.setOnCloseRequest(e -> {
+            System.exit(0);
+        });
+        primaryStage.show();
+    }
+
+    public static void main(String... args) {
+        launch(args);
+    }
+
+    public static Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+}
---
+++ stu_reg_sys/src/main/java/com/curtisnewbie/main/Launcher.java
@@ -0,0 +1,17 @@
+package com.curtisnewbie.main;
+
+/**
+ * ------------------------------------
+ * 
+ * Author: Yongjie Zhuang
+ * 
+ * ------------------------------------
+ * <p>
+ * Launcher with Main method that starts the Javafx Application
+ * </p>
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
