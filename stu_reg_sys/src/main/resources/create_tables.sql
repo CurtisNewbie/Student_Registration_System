ee8bd24e68693bfbb1144b010bd0a4b65a1b6cdc
CurtisNewbie <zhuangyongj@gmail.com>
2020-03-31 09:03:12 +0100

    Update DB design & sql scripts

--- stu_reg_sys/src/main/resources/create_tables.sql
+++ stu_reg_sys/src/main/resources/create_tables.sql
@@ -10,12 +10,34 @@ CREATE TABLE IF NOT EXISTS school(
     CONSTRAINT FOREIGN KEY school_fk_sch (fac_fk) REFERENCES faculty(id)
 );
 
+CREATE TABLE IF NOT EXISTS lecturer(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS module(
+    id INT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    credit SMALLINT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS course(
     id INT PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
     credit SMALLINT NOT NULL,
     sch_fk INT,
-    CONSTRAINT FOREIGN KEY course_fk_sch (sch_fk) REFERENCES school(id)
+    lec_fk INT,
+    CONSTRAINT FOREIGN KEY course_fk_sch (sch_fk) REFERENCES school(id),
+    CONSTRAINT FOREIGN KEY course_fk_lec (lec_fk) REFERENCES lecturer(id)
+);
+
+CREATE TABLE IF NOT EXISTS lecturer_module(
+    lec_fk INT,
+    mod_fk INT,
+    PRIMARY KEY (lec_fk, mod_fk),
+    CONSTRAINT FOREIGN KEY lecmod_fk_lec (lec_fk) REFERENCES lecturer (id),
+    CONSTRAINT FOREIGN KEY lecmod_fk_mod (mod_fk) REFERENCES module (id)
 );
 
 CREATE TABLE IF NOT EXISTS student(
@@ -25,12 +47,6 @@ CREATE TABLE IF NOT EXISTS student(
     reg_date DATE NOT NULL,
     cou_fk INT,
     CONSTRAINT FOREIGN KEY student_fk_cou (cou_fk) REFERENCES course(id)
-);
-
-CREATE TABLE IF NOT EXISTS module(
-    id INT PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL,
-    credit SMALLINT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS student_module(
--- stu_reg_sys/src/main/resources/drop_tables.sql
+++ stu_reg_sys/src/main/resources/drop_tables.sql
@@ -1,7 +1,9 @@
 DROP TABLE student_module;
 DROP TABLE course_module;
+DROP TABLE lecturer_module;
 DROP TABLE student;
 DROP TABLE module;
 DROP TABLE course;
+DROP TABLE lecturer;
 DROP TABLE school;
 DROP TABLE faculty;
