f92a90b234b68392f9ccec21a00faa5ca16cf5ae
CurtisNewbie <zhuangyongj@gmail.com>
2020-03-31 08:31:49 +0100

    Add Course model

---
+++ stu_reg_sys/src/main/java/com/curtisnewbie/model/Course.java
@@ -0,0 +1,66 @@
+package com.curtisnewbie.model;
+
+/**
+ * ------------------------------------
+ * 
+ * Author: Yongjie Zhuang
+ * 
+ * ------------------------------------
+ * <p>
+ * Course representation
+ * </p>
+ */
+public class Course {
+
+    private int id;
+    private String name;
+    private int credit;
+
+    public Course(int id, String name, int credit) {
+        this.id = id;
+        this.name = name;
+        this.credit = credit;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the credit
+     */
+    public int getCredit() {
+        return credit;
+    }
+
+    /**
+     * @param credit the credit to set
+     */
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+}
