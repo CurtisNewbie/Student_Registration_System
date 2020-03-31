44ae64171b5da6c9fe877980d6ffeeb3a2891b59
CurtisNewbie <zhuangyongj@gmail.com>
2020-03-31 09:32:59 +0100

    Add Lecturer model

---
+++ stu_reg_sys/src/main/java/com/curtisnewbie/model/Lecturer.java
@@ -0,0 +1,90 @@
+package com.curtisnewbie.model;
+
+/**
+ * ------------------------------------
+ * 
+ * Author: Yongjie Zhuang
+ * 
+ * ------------------------------------
+ * <p>
+ * Lecturer representation
+ * </p>
+ */
+public class Lecturer {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String position;
+
+    /**
+     * 
+     * @param id       id, set to {@link Dao#GENERATED_ID} if it needs to be
+     *                 auto-generated.
+     * @param fname    firstname
+     * @param lname    lastname
+     * @param position position
+     */
+    public Lecturer(int id, String fname, String lname, String position) {
+        this.id = id;
+        this.firstname = fname;
+        this.lastname = lname;
+        this.position = position;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the firstname
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * @param firstname the firstname to set
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * @return the lastname
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * @param lastname the lastname to set
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    /**
+     * @return the position
+     */
+    public String getPosition() {
+        return position;
+    }
+
+    /**
+     * @param position the position to set
+     */
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
