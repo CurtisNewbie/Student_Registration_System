2e77f15b43fc3fc8f0952fed67a94d2c23fd2ec1
CurtisNewbie <zhuangyongj@gmail.com>
2020-04-01 05:43:23 +0100

    Add unit test for LecturerDao#getAll()

--- stu_reg_sys/src/main/resources/test_data.sql
+++ stu_reg_sys/src/main/resources/test_data.sql
@@ -1,4 +1,9 @@
 -- Change it with caution! It may break the tests
-INSERT INTO student VALUES(1, "Curtis", "Newbie", "2020-01-21", NULL),
+INSERT INTO student VALUES 
+    (1, "Curtis", "Newbie", "2020-01-21", NULL),
     (2, "Sharon", "Lalala", "2020-02-01", NULL),
     (3, "to_be_deleted", "nonono", "2020-03-01", NULL);
+
+INSERT INTO lecturer VALUES 
+    (1, "John", "Cena", "Pricipal Lecturer"),
+    (2, "Apple", "Juice", "Senior Lecturer");
---
+++ stu_reg_sys/src/test/java/com/curtisnewbie/dao/LecturerDaoTest.java
@@ -0,0 +1,47 @@
+package com.curtisnewbie.dao;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import com.curtisnewbie.model.Lecturer;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * ------------------------------------
+ * 
+ * Author: Yongjie Zhuang
+ * 
+ * ------------------------------------
+ * <p>
+ * Unit tests for {@link LecturerDao}
+ * </p>
+ */
+public class LecturerDaoTest {
+
+    /** the id of a Lecturer instance that should be read-only */
+    private final int READ_ONLY_ID = 1;
+    /** the id of a Lecturer instance that is expected to be modified */
+    private final int MODIFIED_ID = 2;
+    /** the id of a Lecturer instance that is expected to be deleted */
+    private final int DELETED_ID = 3;
+    /** the id of a Lecturer instance that will be created */
+    private final int CREATED_ID = 10;
+
+    // private final String FNAME = "John";
+    // private final String LNAME = "Cena";
+    private final LecturerDao dao = new LecturerRepository();
+
+    @BeforeAll
+    static void insertTestData() {
+        TestDBInit.init();
+    }
+
+    @Test
+    void shouldGetAllLecturers() {
+        List<Lecturer> lects = dao.getAll();
+        assertTrue(lects.size() > 0);
+    }
+}
